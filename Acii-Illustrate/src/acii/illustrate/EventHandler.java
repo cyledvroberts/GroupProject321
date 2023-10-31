0bbdacf8e79dbd3b6e8e93a3c57003bcdd5fb5bb
cyledvroberts <cdr0025@uah.edu>
2023-10-30 22:39:32 -0500

    updated illustrate

---
+++ Acii-Illustrate/src/acii/illustrate/EventHandler.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package acii.illustrate;
+
+/**
+ *
+ * @author Cyle
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+public class EventHandler
+{
+    public EventHandler()
+    {
+        
+    }
+            
+}
